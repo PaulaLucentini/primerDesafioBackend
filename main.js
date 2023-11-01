9934650bf5da22da0358c7d7361e1db635b5f9c2
PaulaLucentini <mpaulalucentini@hotmail.com>
2023-11-01 15:51:52 -0300

    correccion con mÃétodos

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+//creo clase producto
+
 class Product {
   constructor(
       title,
@@ -16,17 +19,42 @@ class Product {
   }
 }
 
+//uso la clase de molde y creo un producto
+
+let producto1 = new Product ("producto1", "es un producto", 300, "foto", 1, 10);
+
+//creo clase product manager
+
 class ProductManager {
   constructor(){
       this.product = [];
+      this.contador = 1; //para incrementar
   }
   getProducts(){
-
+    console.log(this.products);
   }
   getProductsById(id){
-
+    let findbyId = this.product.find (producto => producto.id == id)
+    console.log(findbyId);
+    return findbyId;
   }
   addProduct(product){
+    this.product.push(product);
 
+    let productoCode = this.product.find (producto => producto.code == product.code)
+    if(productoCode){
+      console.log("code repetido")
+      return
+    }
+
+    product.id = this.contador //incrementador
+    this.contador++
   }
 }
+
+const productManager = new ProductManager
+
+productManager.addProduct(producto1);
+
+productManager.getProducts()
+
